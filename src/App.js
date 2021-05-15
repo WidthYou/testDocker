8346758098111ab1fd2a6ae52dd7aeba7bb71d6e
sunzp <sunzp@neocrm.com>
2021-05-15 11:41:09 +0800

    fix: test

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ export default () => {
   return (
     <Fragment>
       {
+        // transitionEnd ? <WrapEyesCom /> : <LoginCom transitionEnd={transitionEnd}/>
         transitionEnd ? WrapEyesCom() : <LoginCom transitionEnd={transitionEnd}/>
       }
     </Fragment>
